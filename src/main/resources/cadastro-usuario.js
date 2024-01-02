e06e25ed3a173f20ab0e10d9b6b5e4c7fa613f47
Marianna <109101887+mariannacnsilva@users.noreply.github.com>
2024-01-01 21:03:43 -0300

    Add files via upload

---
+++ src/main/resources/cadastro-usuario.js
@@ -0,0 +1,29 @@
+/*const formularioAcademia = document.querySelector("form");
+const name = document.querySelector("exampleFirstName");
+const cnpj = document.querySelector("exampleInputCNPJ");
+const email = document.querySelector("exampleInputEmail");
+const nUsuarios = document.querySelector("exampleNumeroUsuarios");
+const endereco = document.querySelector("exampleInputEndereco");
+const horaAbertura = document.querySelector("exampleHorarioAbertura");
+const horaFechamento = document.querySelector("exampleHorarioFechamento");
+const senha = document.querySelector("exampleInputPassword");
+const confirmacaoSenha = document.querySelector("exampleRepeatPassword");
+
+formularioAcademia.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const dados = {
+        nome: Inome.value,
+        cnpj: Incnpj.value,
+        email: Iemail.value,
+        num_usuarios: InumUsuarios.value,
+        endereco: Iendereco.value,
+        horario_abertura: IhoraAbertura.value,
+        horario_fechamento: IhoraFechamento.value,
+        senha: Isenha.value,
+        confirmar_senha: IconfirmacaoSenha.value
+        };
+        console.log(dados)
+});*/
+
+// Função para mapa
