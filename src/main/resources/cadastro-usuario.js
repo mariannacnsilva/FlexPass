d91cdbda12c65d992db02cc272ee4af4966ce53b
Marianna <109101887+mariannacnsilva@users.noreply.github.com>
2024-01-03 11:40:59 -0300

    Update cadastro-usuario.js

--- src/main/resources/cadastro-usuario.js
+++ src/main/resources/cadastro-usuario.js
@@ -1,29 +1,8 @@
-/*const formularioAcademia = document.querySelector("form");
-const name = document.querySelector("exampleFirstName");
-const cnpj = document.querySelector("exampleInputCNPJ");
-const email = document.querySelector("exampleInputEmail");
-const nUsuarios = document.querySelector("exampleNumeroUsuarios");
-const endereco = document.querySelector("exampleInputEndereco");
-const horaAbertura = document.querySelector("exampleHorarioAbertura");
-const horaFechamento = document.querySelector("exampleHorarioFechamento");
-const senha = document.querySelector("exampleInputPassword");
-const confirmacaoSenha = document.querySelector("exampleRepeatPassword");
+var app = angular.module("app", []);
 
-formularioAcademia.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const dados = {
-        nome: Inome.value,
-        cnpj: Incnpj.value,
-        email: Iemail.value,
-        num_usuarios: InumUsuarios.value,
-        endereco: Iendereco.value,
-        horario_abertura: IhoraAbertura.value,
-        horario_fechamento: IhoraFechamento.value,
-        senha: Isenha.value,
-        confirmar_senha: IconfirmacaoSenha.value
-        };
-        console.log(dados)
-});*/
-
-// Função para mapa
+app.controller('usuarioController', function($scope, $http, $location) {
+    $scope.registrar = function () {
+        var url = $location.absUrl() + "usuario";
+        $scope.post('createUsuario/')
+    }
+});
